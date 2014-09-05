65f5b2fe1041e42fc49fbd82f3dcdb4d8a8ad997
Matthew Gumport <tropicalmug@gmail.com>
2014-09-05 13:22:19 -0700

    .edn for stopwords.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://clojars.org/teaser-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :resource-paths ["resources"]
   :dependencies [[enlive "1.1.5"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/data.priority-map "0.0.5"]
